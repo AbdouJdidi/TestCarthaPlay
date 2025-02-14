0b2219ededcba56f8b4323e825bdc5a38706c629
Abderrahmen Jedidi <jdidiabderrahmen1@gmail.com>
2025-02-14 19:49:02 +0100

    change

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,12 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://carthaplay1.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use((req, res, next) => {
   console.log(`Received ${req.method} request at ${req.originalUrl}`);
   next();
 });
